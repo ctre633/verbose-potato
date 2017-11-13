c66d1302bf3d0c2456167b8891fbfb3a7039318f
Chris Trentham <ctre633@mail.midland.edu>
2017-11-13 12:31:49 -0600

    Changed 'def' on variables to actual variable types. removed data and ran file.eachLine on its own.

--- src/main.groovy
+++ src/main.groovy
@@ -2,12 +2,12 @@ import java.text.NumberFormat
 
 def file = new URL("https://gist.githubusercontent.com/anonymous/747d5e3bbc57949d8bfe5fd82f359acb/raw/761277a2dcacafb3c06a1e6d0e405ca252098c09/Employee%2520Records.txt").getText()
 
-def sal
-def topSal
-def nameData
-def name
+Float sal
+Float topSal
+String nameData
+String name
 
-def data = file.eachLine { line ->
+file.eachLine { line ->
     if (!line.contains('::EXT::')){
         nameData = line.substring(0,20).trim()
     }
